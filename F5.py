49cc6c9bb066a998f06e8ad7b2bec225dad2b5db
valley3 <98638633+hyakuroume@users.noreply.github.com>
2023-05-08 22:42:39 +0900

    Delete F5.py

--- F5.py
+++
@@ -1,9 +0,0 @@
-import pyautogui
-import webbrowser
-
-
-webbrowser.open("https://www.google.com/webhp?hl=ja&sa=X&ved=0ahUKEwiQlIO71cT8AhUSplYBHZyuAXs4FBA8CAg")
-
-for num in range(10):
-    pyautogui.press("f5")
-
